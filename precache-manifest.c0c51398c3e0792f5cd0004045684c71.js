908ee08c72ff93f95b849432ad3d4b5ab56e354b
ialuj <ritsure.julai.jose@gmail.com>
2020-11-12 23:05:31 +0200

    Updates

---
+++ precache-manifest.c0c51398c3e0792f5cd0004045684c71.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f114adcfbec4a97e1242be3560c70b",
+    "url": "/portal-do-sga/index.html"
+  },
+  {
+    "revision": "0cd09cec4c5bbfa9adc5",
+    "url": "/portal-do-sga/static/css/2.69646142.chunk.css"
+  },
+  {
+    "revision": "3d95224464644e27611a",
+    "url": "/portal-do-sga/static/css/main.a5674db9.chunk.css"
+  },
+  {
+    "revision": "0cd09cec4c5bbfa9adc5",
+    "url": "/portal-do-sga/static/js/2.524fa3be.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portal-do-sga/static/js/2.524fa3be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d95224464644e27611a",
+    "url": "/portal-do-sga/static/js/main.1e34510f.chunk.js"
+  },
+  {
+    "revision": "efe653c1f4752b0619a8",
+    "url": "/portal-do-sga/static/js/runtime-main.62ecae15.js"
+  },
+  {
+    "revision": "e3cb05d5e8cb7d671ee653ba8b5cf3ed",
+    "url": "/portal-do-sga/static/media/apoio_utilizador_portal.e3cb05d5.gif"
+  },
+  {
+    "revision": "45d508f5a69289f7a90c84e5899eef1e",
+    "url": "/portal-do-sga/static/media/apresentacao_portal.45d508f5.gif"
+  },
+  {
+    "revision": "29c01339e5982f468a37aa182525646a",
+    "url": "/portal-do-sga/static/media/logo.29c01339.png"
+  }
+]);
